bc2b74e27a801a650a99668aa435f01362b5e6fc
Alistair Willis <alistairrwillis@gmail.com>
2017-09-09 10:26:42 +0900

    remove ping

--- dist/server.js
+++ dist/server.js
@@ -16,6 +16,6 @@ app.listen(port, (err) => {
     }
 });
 
-setInterval(function() {
-  https.get("https://arwl-noughts-and-crosses.herokuapp.com/");
-}, 180000);
+// setInterval(function() {
+//   https.get("https://arwl-noughts-and-crosses.herokuapp.com/");
+// }, 180000);
